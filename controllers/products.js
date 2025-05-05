b637c61944f2a74a5fdc04cdef314cea7dd567f0
cauliflowers33@gmail.com <cauliflowers33@gmail.com>
2025-05-05 21:25:43 +0300

    add file storage

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,4 @@
-const Product = require('../models/product.js')
+const Product = require("../models/product.js");
 
 exports.getAddProduct = (req, res, next) => {
   res.render("add-product", {
@@ -11,19 +11,20 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const product = new Product(req.body.title)
-  product.save()
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
-  const products = new Product.fetchAll()
-  res.render("shop", {
-    prods: products,
-    pageTitle: "Shop",
-    path: "/",
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true,
+  Product.fetchAll((products) => {
+    res.render("shop", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true,
+    });
   });
 };
--- models/product.js
+++ models/product.js
@@ -1,16 +1,45 @@
-const products = []
+const fs = require("fs");
+const path = require("path");
+
+const getProductsFilePath = () => {
+  return path.join(
+    path.dirname(process.mainModule.filename),
+    "data",
+    "products.json"
+  );
+};
+
+const getProductsFromFile = (cb) => {
+  const p = getProductsFilePath();
+
+  fs.readFile(p, "utf-8", (err, fileContent) => {
+    try {
+      const products = JSON.parse(fileContent);
+      cb(products);
+    } catch (parseErr) {
+      cb([]);
+    }
+  });
+};
 
 module.exports = class Product {
-    constructor(title){
-        this.title = title;
-    }
+  constructor(title) {
+    this.title = title;
+  }
 
-    save(){
-        products.push(this)
-    }
+  save() {
+    const p = getProductsFilePath();
 
-    static fetchAll(){
-      return products
-    }
+    getProductsFromFile((products) => {
+      products.push(this);
 
-}
+      fs.writeFile(p, JSON.stringify(products, null, 2), (err) => {
+        if (err) console.log("Помилка при записі:", err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
