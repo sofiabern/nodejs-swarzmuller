e9e110d12b99588cce71b08d1ed41555e9f6857e
cauliflowers33@gmail.com <cauliflowers33@gmail.com>
2025-05-17 23:09:36 +0300

    complete edit product

--- models/product.js
+++ models/product.js
@@ -23,7 +23,8 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -31,15 +32,26 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
+ 
     const p = getProductsFilePath();
 
     getProductsFromFile((products) => {
-      products.push(this);
+      if (this.id) {
+        const exisitingProductIndex = products.findIndex(prod => prod.id === this.id)
+        const updatedProducts = [...products];
+        updatedProducts[exisitingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts, null, 2), (err) => {
+          if (err) console.log("Помилка при записі:", err);
+        });
+      } else {
 
-      fs.writeFile(p, JSON.stringify(products, null, 2), (err) => {
-        if (err) console.log("Помилка при записі:", err);
-      });
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products, null, 2), (err) => {
+          if (err) console.log("Помилка при записі:", err);
+        });
+      }
+
     });
   }
 
