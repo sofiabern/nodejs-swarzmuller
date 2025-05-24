b9deb260f045fde6cfac29c75436020134f52375
cauliflowers33@gmail.com <cauliflowers33@gmail.com>
2025-05-24 14:58:06 +0300

    add orders

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongodb = require("mongodb");
+const { get } = require("../routes/shop.js");
 const getDb = require("../util/database.js").getDb;
 
 class User {
@@ -85,8 +86,41 @@ class User {
       .collection("users")
       .updateOne(
         { _id: new mongodb.ObjectId(this._id) },
-        { $set: { cart: {items: updatedCartItems}} }
+        { $set: { cart: { items: updatedCartItems } } }
       );
+  }
+
+  addOrder() {
+    const db = getDb();
+    return this.getCart()
+      .then((products) => {
+        const order = {
+          items: products,
+          user: {
+            _id: new mongodb.ObjectId(this._id),
+            name: this.name,
+          },
+        };
+        return db.collection("orders").insertOne(order);
+      })
+      .then((result) => {
+        this.cart = { items: [] };
+        return db
+          .collection("users")
+          .updateOne(
+            { _id: new mongodb.ObjectId(this._id) },
+            { $set: { cart: { items: [] } } }
+          );
+      });
+  }
+
+  getOrders() {
+    const db = getDb();
+
+    return db
+      .collection("orders")
+      .find({ "user._id": new mongodb.ObjectId(this._id) })
+      .toArray()
   }
 
   static findById(userId) {
