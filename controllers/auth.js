df9aa895a01d1319baf964d9c192222020aa5a42
cauliflowers33@gmail.com <cauliflowers33@gmail.com>
2025-06-14 01:37:43 +0300

    add email validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcryptjs");
 const nodemailer = require("nodemailer");
 const sendgridTransport = require("nodemailer-sendgrid-transport");
 const dotenv = require("dotenv")
+const {validationResult} = require('express-validator')
 
 dotenv.config();
 
@@ -80,6 +81,16 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+
+if(!errors.isEmpty()){
+  console.log(errors.array())
+  return res.status(422).render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
+    errorMessage: errors.array()[0].msg,
+  });
+}
 
   User.findOne({ email: email })
     .then((userDoc) => {
