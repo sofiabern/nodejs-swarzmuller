703be3a725aae76f9cb4a303ab5f367b54261f3c
cauliflowers33@gmail.com <cauliflowers33@gmail.com>
2025-06-14 01:56:29 +0300

    add password check

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const { check } = require("express-validator");
+const { check, body } = require("express-validator");
 
-const authController = require('../controllers/auth.js')
+const authController = require("../controllers/auth.js");
 
 const router = express.Router();
 
@@ -11,7 +11,33 @@ router.get("/signup", authController.getSignup);
 
 router.post("/login", authController.postLogin);
 
-router.post("/signup", check('email').isEmail().withMessage("Please enter a valid email"), authController.postSignup);
+router.post(
+  "/signup",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid email")
+      .custom((value, { req }) => {
+        if (value === "test@test.com") {
+          throw new Error("This email address is forbidden");
+        }
+        return true;
+      }),
+    body(
+      "password",
+      "Please enter a password with only numbers and text and at least 5 characters"
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+    body('confirmPassword').custom((value, {req}) => {
+       if(value = req.body.password){
+        throw new Error('Passwords have to match!')
+       }
+       return true
+    })
+  ],
+  authController.postSignup
+);
 
 router.post("/logout", authController.postLogout);
 
@@ -23,4 +49,4 @@ router.get("/reset/:token", authController.getNewPassword);
 
 router.post("/new-password", authController.postNewPassword);
 
-module.exports = router
+module.exports = router;
