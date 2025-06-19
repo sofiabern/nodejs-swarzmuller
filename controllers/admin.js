8db5157313bdece87af0f84b9802f9fe2c44632f
cauliflowers33@gmail.com <cauliflowers33@gmail.com>
2025-06-20 00:11:54 +0300

    enhance product delete

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,4 @@
+const fileHelper = require("../util/file");
 const Product = require("../models/product.js");
 const { validationResult } = require("express-validator");
 
@@ -21,7 +22,7 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
 
-  if(!image){
+  if (!image) {
     return res.status(422).render("admin/edit-product", {
       pageTitle: "Add Product",
       path: "/admin/add-product",
@@ -39,21 +40,21 @@ exports.postAddProduct = (req, res, next) => {
 
   const errors = validationResult(req);
 
-    if(!errors.isEmpty()) {
+  if (!errors.isEmpty()) {
     console.log(errors.array());
-   return res.status(422).render("admin/edit-product", {
-     pageTitle: "Add Product",
-     path: "/admin/add-product",
-     editing: false,
-     hasError: true,
-     product: {
-       title: title,
-       price: price,
-       description: description,
-     },
-     errorMessage: errors.array()[0].msg,
-     validationErrors: errors.array(),
-   });
+    return res.status(422).render("admin/edit-product", {
+      pageTitle: "Add Product",
+      path: "/admin/add-product",
+      editing: false,
+      hasError: true,
+      product: {
+        title: title,
+        price: price,
+        description: description,
+      },
+      errorMessage: errors.array()[0].msg,
+      validationErrors: errors.array(),
+    });
   }
 
   const imageUrl = image.path;
@@ -73,9 +74,9 @@ exports.postAddProduct = (req, res, next) => {
       res.redirect("/admin/products");
     })
     .catch((err) => {
-      const error = new Error(err)
-      error.httpStatusCode = 500
-      return next(error)
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
 
@@ -143,9 +144,10 @@ exports.postEditProduct = (req, res, next) => {
       product.title = updatedTitle;
       product.price = updatedPrice;
       product.description = updatedDesc;
-if(image){
-  product.imageUrl = image.path
-}
+      if (image) {
+        fileHelper.deleteFile(product.imageUrl);
+        product.imageUrl = image.path;
+      }
       return product.save().then(() => {
         console.log("UPDATED PRODUCT!");
         res.redirect("/admin/products");
@@ -182,8 +184,14 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-
-  Product.deleteOne({ _id: prodId, userId: req.user._id })
+  Product.findById(prodId)
+    .then((product) => {
+      if (!product) {
+        return next(new Error("Product not found"));
+      }
+      fileHelper.deleteFile(product.imageUrl);
+      return Product.deleteOne({ _id: prodId, userId: req.user._id });
+    })
     .then(() => {
       console.log("DELETED PRODUCT!");
       res.redirect("/admin/products");
