c87eb52972f187e5e2271e73ee40ccb7928cd343
cauliflowers33@gmail.com <cauliflowers33@gmail.com>
2025-07-01 23:50:07 +0300

    add io creatre and update

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 const { validationResult } = require("express-validator");
+
+const io = require('../socket')
 const Post = require("../models/post");
 const User = require("../models/user.js");
 
@@ -12,6 +14,7 @@ exports.getPosts = async (req, res, next) => {
     const totalItems = await Post.find().countDocuments();
     const posts = await Post.find()
       .populate("creator")
+      .sort({createdAt: -1})
       .skip((currentPage - 1) * perPage)
       .limit(perPage);
 
@@ -57,6 +60,9 @@ exports.createPost = async (req, res, next) => {
 
     user.posts.push(post._id);
     await user.save();
+
+    io.getIO().emit('posts', {action: 'create', post: post })
+
     res.status(201).json({
       message: "Post created succesfully!",
       post: post,
@@ -108,14 +114,14 @@ exports.updatePost = async (req, res, next) => {
 
 
   try {
-    const post = await Post.findById(postId);
+    const post = await Post.findById(postId).populate('creator');
     if (!post) {
       const error = new Error("Could not find post.");
       error.statusCode = 404;
       throw error;
     }
     
-  if (post.creator.toString() !== req.userId) {
+  if (post.creator._id.toString() !== req.userId) {
     const error = new Error("Not authorized");
     error.statusCode = 403;
     throw error;
@@ -134,6 +140,7 @@ exports.updatePost = async (req, res, next) => {
     post.imageUrl = imageUrl;
     post.content = content;
     const result = await post.save();
+    io.getIO().emit('posts', {action: "update", post: result})
     res.status(200).json({ message: "Post updated!", post: result });
   } catch (err) {
     if (!err.statusCode) {
