a48575c9d6c332c27b80641d0c9e53479eeecae3
cauliflowers33@gmail.com <cauliflowers33@gmail.com>
2025-07-06 20:15:46 +0300

    add login

--- app.js
+++ app.js
@@ -40,6 +40,10 @@ app.use((req, res, next) => {
     "GET, POST, PUT, PATCH, DELETE"
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if(req.method === 'OPTIONS'){
+    return res.sendStatus(200)
+  }
   next();
 });
 
--- graphql/schema.js
+++ graphql/schema.js
@@ -20,6 +20,11 @@ status: String!
 posts: [Post!]!
 }
 
+type AuthData{
+token: String!
+userId: String!
+}
+
 input UserInputData {
   email: String!
   name: String!
@@ -27,7 +32,7 @@ input UserInputData {
 }
 
 type RootQuery {
-hello: String
+login(email: String!, password: String!): AuthData!
 }
 
 type RootMutation {
