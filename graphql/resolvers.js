7ba5f1e4c1f52f09e4c23f32e02a2062949146b5
cauliflowers33@gmail.com <cauliflowers33@gmail.com>
2025-07-08 05:06:55 +0300

    add user and status routes

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -256,5 +256,36 @@ module.exports = {
     return true
   },
 
-  
+  user: async function(args, req){
+    if (!req.isAuth) {
+      const error = new Error("Not authenticated!");
+      error.code = 401;
+      throw error;
+    }
+    const user = await User.findById(req.userId)
+    if(!user){
+      const error = new Error("No user found!");
+      error.code = 404;
+      throw console.error();
+    }
+
+    return {...user._doc, _id: user._id.toString()}
+  },
+  updateStatus: async function({status}, req){
+    if (!req.isAuth) {
+      const error = new Error("Not authenticated!");
+      error.code = 401;
+      throw error;
+    }
+    const user = await User.findById(req.userId)
+    if (!user) {
+      const error = new Error("No user found!");
+      error.code = 404;
+      throw console.error();
+    }
+
+    user.status = status;
+    await user.save()
+    return {...user._doc, _id: user._id.toString()}
+  }
 };
