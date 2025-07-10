b8d2b2b81e947ba6162356e1939384a9dcfa86d3
cauliflowers33@gmail.com <cauliflowers33@gmail.com>
2025-07-10 03:44:16 +0300

    add ssl

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const fs = require('fs')
+const https = require('https')
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -25,6 +26,9 @@ const store = new MongoDBStore({
 });
 
 const csrfProtection = csrf();
+
+const privateKey = fs.readFileSync('server.key')
+const certificate = fs.readFileSync('server.certificate')
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -126,7 +130,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGODB_URI)
   .then(() => {
-    app.listen(process.env.PORT || 3000);
+    https.createServer({key: privateKey, cert: certificate}, app).listen(process.env.PORT || 3000);
   })
   .catch((err) => {
     console.log(err);
