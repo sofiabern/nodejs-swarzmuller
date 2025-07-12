dab93c4ce92e8813c63e0cb9131fc92dad41d376
cauliflowers33@gmail.com <cauliflowers33@gmail.com>
2025-07-12 04:39:13 +0300

    it func

---
+++ test/auth-middleware.js
@@ -0,0 +1,22 @@
+const expect = require("chai").expect;
+const authMiddleware = require("../middleware/is-auth.js");
+
+it("should throw an error if no authorization header is present", function () {
+  const req = {
+    get: function () {
+      return null;
+    },
+  };
+
+  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw("Not authenticated.");
+});
+
+it('should throw an error if the authorization header is only one string', function (){
+     const req = {
+       get: function () {
+         return 'xyz';
+       },
+     };
+
+  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+})
