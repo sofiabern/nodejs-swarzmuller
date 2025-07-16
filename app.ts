466d00727a9e70bb2e966daa4abb2c2c4cb355d8
cauliflowers33@gmail.com <cauliflowers33@gmail.com>
2025-07-16 22:05:06 +0300

    compare server spin up

--- app.ts
+++ app.ts
@@ -1,9 +1,3 @@
-const text = "This is a test = and it should be stored in a file!"
-
-const encoder = new TextEncoder()
-const data = encoder.encode(text)
-
-Deno.writeFile('message.txt', data)
-.then(() => {
-    console.log('Wrote to file!')
-})
+Deno.serve({ port: 3000 }, (_req) => {
+  return new Response("Hello, World!");
+});
